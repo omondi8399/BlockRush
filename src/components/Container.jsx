17e9a206c13a98f42d2065bc114067ee60e9a618
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-07-16 15:16:15 +0300

    Updated Container component

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -4,22 +4,22 @@ const Container = () => {
   return (
     <div>
         <div className='container'>
-        <div className='text'>
-            <h3>WEB3 Experience</h3>
-            <h1>BLOCKRUSH AUTOMATION</h1>
-            <p>BLOCKRUSH is a web3 automated market aggregator and multi-chain batch miner. BLOCKRUSH was created with the goal of creating an experience... </p>
-            <div className='button'><a href='/'>Explore Now  &#8599;</a></div>
-        </div>
-        <div className='image'>
-            <img src='crypto1.png' alt="" />
-        </div>
+            <div className='text'>
+                <h3>WEB3 Experience</h3>
+                <h1>BLOCKRUSH AUTOMATION</h1>
+                <p>BLOCKRUSH is a web3 automated market aggregator and multi-chain batch miner. BLOCKRUSH was created with the goal of creating a seamless and frictionless experience...</p>
+                <div className='button'><a href="/">Explore Now &#8599;</a></div>
+            </div>
+            <div className='image'>
+                <img src="crypto1.png" alt="" />
+            </div>
         </div>
         <div className='partners'>
-            <span><img src='webflow.png' alt=''/></span>
-            <span><img src='mparticle.png' alt=''/></span>
-            <span><img src='chargebee.png' alt=''/></span>
-            <span><img src='corsair.png' alt=''/></span>
-            <span><img src='emachines.png' alt=''/></span>
+            <span><img src="webflow.png" alt="" /></span>
+            <span><img src="mparticle.png" alt="" /></span>
+            <span><img src="chargebee.png" alt="" /></span>
+            <span><img src="corsair.svg" alt="" /></span>
+            <span><img src="emachines.png" alt="" /></span>
         </div>
     </div>
   )
