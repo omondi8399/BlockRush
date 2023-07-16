313a42e1e789399bec3a7927ab0358d381b8f76a
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-07-16 15:14:56 +0300

    Updated Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import NavBar from './NavBar'
-import './style.css'
+import Navbar from './NavBar'
+import './styles.css'
 import Container from './Container'
 import Section from './Section'
 import Testimonials from './Testimonials'
@@ -8,16 +8,16 @@ import FAQ from './FAQ'
 import Footer from './Footer'
 
 const Home = () => {
-    return (
-        <div className='home'>
-            <NavBar />
-            <Container />
-            <Section />
-            <Testimonials />
-            <FAQ />
-            <Footer />
-        </div>
-    )
+return (
+    <div className='home'>
+        <Navbar/>
+        <Container/>
+        <Section/>
+        <Testimonials/>
+        <FAQ/>
+        <Footer/>
+    </div>
+)
 }
 
 export default Home
