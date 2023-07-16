36d27289835030b99da92dca0a00d338b6a527fe
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-07-16 15:15:39 +0300

    Updated FAQ component

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -4,28 +4,55 @@ const FAQ = () => {
   return (
     <div className='faq'>
         <h1>Frequently Asked Questions</h1>
-        <p>Have Questions? We are here to help</p>
+        <p>Have question? We are here to help</p>
         <section className='services'>
             <div className='service-right'>
                 <div className='qa'>
-                    <input type='checkbox' id='collapsible-head-4' />
-                    <label for="collapsible-head-4">What chain do BlockRush support?</label>
-                    <div class="collapsible-text-4">
-                        <p>LLorem ipsum....Add something later</p>
+                    <input type="checkbox" id='collapsible-head-1' />
+                    <label for = "collapsible-head-1">What is the price?</label>
+                    <div className='collapsible-text-1'>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit odit similique doloremque
+                            ab unde. Placeat recusandae nobis voluptate atque, fugit itaque nam quos.</p>
                     </div>
                 </div>
                 <div className='qa'>
-                    <input type='checkbox' id='collapsible-head-4' />
-                    <label for="collapsible-head-4">What chain do BlockRush support?</label>
-                    <div class="collapsible-text-4">
-                        <p>LLorem ipsum....Add something later</p>
+                <input type="checkbox" id="collapsible-head-2" />
+                <label for="collapsible-head-2">How many keys are there?</label>
+                <div class="collapsible-text-2">
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit odit similique doloremque
+                        ab unde. Placeat recusandae nobis voluptate atque, fugit itaque nam quos.</p>
+                </div>
+                </div>
+                <div className='qa'>
+                    <input type="checkbox" id="collapsible-head-3" />
+                    <label for="collapsible-head-3">Are keys unbindable?</label>
+                    <div class="collapsible-text-3">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit odit similique doloremque
+                            ab unde. Placeat recusandae nobis voluptate atque, fugit itaque nam quos.</p>
                     </div>
                 </div>
                 <div className='qa'>
-                    <input type='checkbox' id='collapsible-head-4' />
-                    <label for="collapsible-head-4">What chain do BlockRush support?</label>
+                    <input type="checkbox" id="collapsible-head-4" />
+                    <label for="collapsible-head-4">What chain do BLOCKRUSH support?</label>
                     <div class="collapsible-text-4">
-                        <p>LLorem ipsum....Add something later</p>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit odit similique doloremque
+                            ab unde. Placeat recusandae nobis voluptate atque, fugit itaque nam quos.</p>
+                    </div>
+                </div>
+                <div className='qa'>
+                    <input type="checkbox" id="collapsible-head-5" />
+                    <label for="collapsible-head-5">What OS is TN compatible with?</label>
+                    <div class="collapsible-text-5">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit odit similique doloremque
+                            ab unde. Placeat recusandae nobis voluptate atque, fugit itaque nam quos.</p>
+                    </div>
+                </div>
+                <div className='qa'>
+                    <input type="checkbox" id="collapsible-head-6" />
+                    <label for="collapsible-head-6">Is BLOCKRUSH safe to use?</label>
+                    <div class="collapsible-text-6">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit odit similique doloremque
+                            ab unde. Placeat recusandae nobis voluptate atque, fugit itaque nam quos.</p>
                     </div>
                 </div>
             </div>
