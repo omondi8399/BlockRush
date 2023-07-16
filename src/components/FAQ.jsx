025138091d7ee2a75726932f2e4f2e44734ba025
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-07-16 11:15:30 +0300

    Added FAQ component

---
+++ src/components/FAQ.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const FAQ = () => {
+  return (
+    <div className='faq'>
+        <h1>Frequently Asked Questions</h1>
+        <p>Have Questions? We are here to help</p>
+        <section className='services'>
+            <div className='service-right'>
+                <div className='qa'>
+                    <input type='checkbox' id='collapsible-head-4' />
+                    <label for="collapsible-head-4">What chain do BlockRush support?</label>
+                    <div class="collapsible-text-4">
+                        <p>LLorem ipsum....Add something later</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+  )
+}
+
+export default FAQ
