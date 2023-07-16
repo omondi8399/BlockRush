794dbaec445db27d89df2d5b3858c6f3ab590efb
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-07-16 15:15:20 +0300

    Updated Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,24 +5,24 @@ const Footer = () => {
     <div className='footer'>
         <div className='bulb'>
             <div className='pic'>
-                <img src='bulb.png' alt='' />
-                <h1>Knowledge Grow your Influence</h1>
+                <img src="bulb.png" alt="" />
+                <h1>Knowledge Grow Your Influence</h1>
             </div>
             <div className='footer-text'>
-                <p>Lerem ipsum</p>
-                <a href='/'>Get Started</a>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis quaerat in error, pariatur optio dolorum corporis dolore maiores suscipit. Quam.</p>
+                <a href="/">Get Started</a>
             </div>
         </div>
         <div className='newsletter'>
             <div className='news-left'>
                 <h4>Subscribe</h4>
-                <h1>Subscribe to Get Latest <br /> Update From Us</h1>
-                <span className='curve'><img src='curve.png' alt='' /></span>
+                <h1>Subscribe To Get Latest <br />Update From Us</h1>
+                <span className='curve'><img src="curve.png" alt="" /></span>
             </div>
             <div className='news-right'>
-                <p>Lorem ipsum</p>
-                <form action=''>
-                    <input type='email' name="email" />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia explicabo exercitationem quis consequuntur voluptas nihil.</p>
+                <form action="">
+                    <input type="email" name='email' />
                     <button>Subscribe</button>
                 </form>
             </div>
@@ -30,10 +30,10 @@ const Footer = () => {
         <div className='footer-menu'>
             <h1>BLOCKRUSH</h1>
             <ul className='footer-inner-nav'>
-                <li><a href='#hub'>Hub</a></li>
-                <li><a href='#mint'>Mint</a></li>
-                <li><a href='#features'>Features</a></li>
-                <li><a href='#socials'>Socials</a></li>
+                <li><a href="#hub">Hub</a></li>
+                <li><a href="#mint">Mint</a></li>
+                <li><a href="#features">Features</a></li>
+                <li><a href="#socials">Socials</a></li>
             </ul>
         </div>
         <hr />
